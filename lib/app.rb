fa661c978da58b6c4af890de95bac2181c7ff2d4
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2018-12-14 15:05:09 +0000

    added sessions for users and moved rendering from post

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 
 # Creating our own subclass of Sinatra::Base - Modular application style.
 class Battle < Sinatra::Base
+  enable :sessions
   # Defining route
   get '/' do
     # 'Testing infrastructure working!'
@@ -9,11 +10,16 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
 
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
+  end
   # run! starts a server.
   # Only start a server if the file has been executed directly with __FILE__
   # being the current file and $0 being the executed file.
